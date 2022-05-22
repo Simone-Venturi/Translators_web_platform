0ec905468419f8c94e46e7e66181fe5d4ebca8d9
Simone Venturi <s.venturi@sis-ter.it>
2022-05-22 22:28:09 +0200

    Add routes, controllers Auth, middleware authorization and authentication

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8080"
+};
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const db = require("./models");
+const Role = db.role;
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  require("./seeders/index").runSeeders();
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Hello world!" });
+});
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ backend/middleware/authJwt.js
@@ -0,0 +1,68 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided!"
+    });
+  }
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorized!"
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+isAdmin = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRole().then(role => {
+      if (role.name === "admin"){
+          next();
+          return;
+      }
+      res.status(403).send({
+        message: "Require Admin Role!"
+      });
+      return;
+    });
+  });
+};
+isTranslator = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRole().then(role => {
+      if (role.role_translator === true) {
+        next();
+        return;
+      }
+      res.status(403).send({
+        message: "Require Translator Role!"
+      });
+    });
+  });
+};
+isDataScientist = (req, res, next) => {
+  User.findByPk(req.userId).then(user => {
+    user.getRole().then(role => {
+      if (role.role_data_scientist === true) {
+        next();
+        return;
+      }
+      res.status(403).send({
+        message: "Require Data Scientist Role!"
+      });
+    });
+  });
+};
+const authJwt = {
+  verifyToken: verifyToken,
+  isAdmin: isAdmin,
+  isTranslator: isTranslator,
+  isDataScientist: isDataScientist
+};
+module.exports = authJwt;
