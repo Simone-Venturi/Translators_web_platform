7e7dfb0d23cb4df844aa148b3d254d98a8677f71
Simone Venturi <s.venturi@sis-ter.it>
2022-05-26 12:25:46 +0200

    Add unique constrait in tranlsation

--- backend/models/translation.model.js
+++ backend/models/translation.model.js
@@ -1,12 +1,25 @@
 module.exports = (sequelize, Sequelize) => {
     const Translation = sequelize.define("translations", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
       original: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        allowNull: false,
+        unique: 'translation_unique'
       },
       translated: {
         type: Sequelize.INTEGER,
-        primaryKey: true
+        allowNull: false,
+        unique: 'translation_unique'
+      }
+    }, {
+      uniqueKeys: {
+          translation_unique: {
+              fields: ['original', 'translated']
+          }
       }
     });
     return Translation;
