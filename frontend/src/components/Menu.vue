619aca931bc6d4b5300ab9e2719adf3957633a0a
Simone Venturi <s.venturi@sis-ter.it>
2022-06-16 11:55:55 +0200

    [REFACTOR] Menu. Now menu is a Component

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,69 @@
+<template>    
+    <div class="row ml-4 mr-4">
+      <div class="col-3">
+        <MenuButton class="menubutton" text="Translate" @click="showTranslate"/>
+      </div>
+      <div class="col-3">
+        <MenuButton class="menubutton" text="Review" @click="showReview"/>
+      </div>
+      <div class="col-3">
+        <MenuButton class="menubutton" text="Alignment" @click="showAlignment"/>
+      </div>
+      <div class="col-3">
+        <MenuButton class="menubutton" text="Profile" @click="showProfile"/>
+      </div>
+    </div> 
+</template>
+
+<script>
+import MenuButton from '@/components/MenuButton.vue'
+export default {
+    data() {
+        return {
+            content: null,
+            routesAvailable: ['translate', 'review', 'alignment', 'profile']
+        }
+    },
+    components: {
+        MenuButton
+    },
+    methods: {        
+        showTranslate(event){
+            this.hadSelectedClassToSelectedButton(event)
+        },
+        showReview(event){
+            this.hadSelectedClassToSelectedButton(event)
+        },
+        showAlignment(event){
+            this.hadSelectedClassToSelectedButton(event)
+        },
+        showProfile(event){
+            this.hadSelectedClassToSelectedButton(event)
+        },
+        hadSelectedClassToSelectedButton(event){
+            this.$router.push('/'+event.target.innerText.toLowerCase())
+        },
+        selectCurrentButton(currentRoute){
+            this.content = this.routesAvailable.filter(route => currentRoute.includes(route))[0]
+            document.querySelectorAll(".menubutton").forEach(el => {
+                if(el.innerText.toLowerCase() === this.content){
+                    el.classList.remove("not-selected")
+                    el.classList.add("selected")
+                } else {
+                    el.classList.add("not-selected")
+                    el.classList.remove("selected")
+                }
+            })
+        },
+    },
+    mounted() {
+        this.selectCurrentButton(this.$route.fullPath)
+    }
+}
+</script>
+
+<style scoped>
+  .col-3 {
+    text-align: center;
+  }
+</style>
--- frontend/src/views/Translate.vue
+++ frontend/src/views/Translate.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="container">
-    <div class="row ml-4 mr-4">
-      <div class="col-3">
-        <MenuButton text="Translate" @click="showTranslate"/>
-      </div>
-      <div class="col-3">
-        <MenuButton text="Review" @click="showReview"/>
-      </div>
-      <div class="col-3">
-        <MenuButton text="Alignment" @click="showAlignment"/>
-      </div>
-      <div class="col-3">
-        <MenuButton text="Profile" @click="showProfile"/>
-      </div>
-    </div>    
+    <Menu />
     <div class="row h-100">
       <div class="col-12">
         <p>Translate Sentences from 
@@ -30,7 +17,7 @@
 </template>
 
 <script>
-import MenuButton from '@/components/MenuButton.vue'
+import Menu from '@/components/Menu.vue'
 import Dropdown from '@/components/DropdownLayout.vue';
 import TranslateDataTable from '@/components/TranslateDataTable.vue';
 import LanguageService from "../services/language.service";
@@ -38,7 +25,7 @@ import LanguageService from "../services/language.service";
 export default {
   name: "Translate",
   components: {
-    MenuButton,
+    Menu,
     Dropdown,
     TranslateDataTable
   },
@@ -51,33 +38,6 @@ export default {
     };
   },
   methods:{
-    showTranslate(event){
-      this.hadSelectedClassToSelectedButton(event)
-      this.content = 'Translate'
-    },
-    showReview(event){
-      this.hadSelectedClassToSelectedButton(event)
-      this.content = 'Review'
-    },
-    showAlignment(event){
-      this.hadSelectedClassToSelectedButton(event)
-      this.content = 'Alignment'
-    },
-    showProfile(event){
-      this.hadSelectedClassToSelectedButton(event)
-      this.content = 'Profile'
-    },
-    hadSelectedClassToSelectedButton(event){
-       document.querySelectorAll("button").forEach(el => {
-         if(el === event.target){
-            el.classList.remove("not-selected")
-            el.classList.add("selected")
-         } else {
-            el.classList.add("not-selected")
-            el.classList.remove("selected")
-         }
-      })
-    },
     changeFrom(){
       this.fromLanguageSelected = this.$refs.languageFrom.selected
     },
