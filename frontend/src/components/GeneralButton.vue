70b149bd6579a89db960d3b206226802bd4d0778
Simone Venturi <s.venturi@sis-ter.it>
2022-06-22 11:38:29 +0200

    [FRONTEND] Created GeneralButton component

---
+++ frontend/src/components/GeneralButton.vue
@@ -0,0 +1,32 @@
+<template>    
+    <button>{{text}}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            default: null
+        },
+        colorHover: {
+            type: String,
+            default: '#41b883'
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+  padding: 1% 10%;
+  background: transparent;
+  -webkit-box-shadow: 6px 6px 10px 3px rgba(0,0,0,0.75); 
+  box-shadow: 6px 6px 10px 3px rgba(0,0,0,0.75);
+}
+
+button:hover{
+  color: white;
+  background-color: v-bind(colorHover);
+}
+</style>
