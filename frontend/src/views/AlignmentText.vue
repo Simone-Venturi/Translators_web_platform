6bd68fea3ea6421bb919fc79d8e5923078efe343
Simone Venturi <s.venturi@sis-ter.it>
2022-07-15 11:37:04 +0200

    [FRONTEND] Add boxes for parallel texts

--- frontend/src/views/AlignmentText.vue
+++ frontend/src/views/AlignmentText.vue
@@ -7,6 +7,28 @@
           <h2>Alignment</h2>
         </div>
       </div>
+      <div class="row ml-4 mr-4">
+        <div class="col-6">
+          <h3>Original text:</h3>
+          <p>{{parallelText.originalText}}</p>
+        </div>
+        <div class="col-6">
+          <h3>Translated text:</h3>
+          <p>{{parallelText.translatedText}}</p>
+        </div>
+      </div>
+      <div class="row ml-4 mr-4">
+        <div class="col-12">
+          <Splitter style="height: 300px">
+            <SplitterPanel>
+              <p>{{parallelText.originalText}}</p>
+            </SplitterPanel>
+            <SplitterPanel>
+              <p>{{parallelText.translatedText}}</p>
+            </SplitterPanel>
+          </Splitter>
+        </div>
+      </div>
     </div>      
   </div>
 </template>
@@ -14,17 +36,23 @@
 <script>
 import AlignmentsService from "../services/alignment.service";
 import Menu from '@/components/Menu.vue'
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
 
 export default {
   name: "Translation",
   components: {
-    Menu
+    Menu,
+    Splitter,
+    SplitterPanel
   },
   data() {
     return {
       content: "Alignment",
       parallelText: {},
-    };
+      originalTextList: [],
+      translatedTextList: []
+    }
   },
   methods: {
   },
@@ -32,9 +60,9 @@ export default {
     AlignmentsService.getAlignmentFromID(this.$route.params.idParallelText).then(
       (response) => {
         this.parallelText = response.data;
-        if(this.parallelText.originalLanguage == this.$route.params.idLanguageTo){
-          this.$router.push('/alignment')
-        }
+        let regex = new RegExp(/[^.?!]+[.!?]+[\])'"`’”]*|.+/, 'g');
+        this.originalTextList = this.parallelText.originalText.match(regex)
+        this.translatedTextList = this.parallelText.translatedText.match(regex)
       },
       (error) => {
         console.log(error)
