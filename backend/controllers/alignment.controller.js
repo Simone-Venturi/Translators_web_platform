f72620da72f0ff6db3791a207e80e7976b11ee81
Simone Venturi <s.venturi@sis-ter.it>
2022-07-19 15:51:14 +0200

    [BACKEND] Add alignment available route

--- backend/controllers/alignment.controller.js
+++ backend/controllers/alignment.controller.js
@@ -30,6 +30,36 @@ exports.allAlignments = (req, res) => {
     })
 }
 
+exports.allAlignmentsAvailable = (req, res) => {
+    ParallelText.findAll({
+        attributes: ['id', 'originalText', 'translatedText', 'originalLanguage', 'translatedLanguage'],
+        include: ['Translations'],
+        where: {
+            '$Translations.id$': { [db.Sequelize.Op.eq]: null },
+        }
+    }).then( parrallelTexts => {        
+        if (!parrallelTexts) {
+            return res.status(404).send({ message: "ParallelTexts not found." });
+        }
+        Language.findAll({
+            attributes: ['idlanguage', 'title', 'abbreviation'],
+            include: ['TranslatorTranslateLanguage'],
+            where: {
+                '$TranslatorTranslateLanguage.id$': { [db.Sequelize.Op.eq]: req.userId }
+            }
+        }).then( languages => {
+            if (!languages) {
+                return res.status(404).send({ message: "Languages not found." });
+            }
+            let array_id_language = languages.map(language => language.idlanguage)
+            parrallelTexts = parrallelTexts
+                .filter(parallel_text => array_id_language.includes(parallel_text.originalLanguage))
+                .filter(parallel_text => array_id_language.includes(parallel_text.translatedLanguage))
+            res.status(200).send(parrallelTexts)
+        });
+    })
+}
+
 exports.getParallelTextFromID = (req, res) => {
     ParallelText.findByPk(req.params.parallelText)
         .then( parallelText => {
@@ -66,7 +96,9 @@ exports.createAlignment = (req, res) => {
                         Translation.create({
                             original: sentenceOriginal.idsentence,
                             translated: sentenceTranslated.idsentence,
-                            translator: req.userId
+                            translator: req.userId,
+                            parallel_text_id: parallelText.id,
+                            is_generated_from_alignment: true
                         }).then( translation => {
                             if(!translation){
                                 return res.status(404).send({ message: "Translation not created." });
