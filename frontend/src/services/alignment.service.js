8fd5072e4b785e2d58dee6034a569876c9d6cc62
Simone Venturi <s.venturi@sis-ter.it>
2022-07-20 11:53:29 +0200

    [FEATURE] translationObjectsArray creates only translation without white spaces in the beginning of sentence

--- frontend/src/services/alignment.service.js
+++ frontend/src/services/alignment.service.js
@@ -8,6 +8,26 @@ class AlignmentsService {
     return ApiClient.get(API_GET_PARALLEL_TEXT_FROM_ID_ENDPOINT + '/' + id, { headers: authHeader() });
   }
   createAlignment(idParallelText, translationObjectsArray, sentenceArray){
+    //White spaces are removed from first position
+    translationObjectsArray = translationObjectsArray.map(element => {
+      if(element.original_sentence[0] == ' '){
+        return {
+          original_sentence: element.original_sentence.substring(1),
+          translated_sentence: element.translated_sentence
+        }
+      } else {
+        return element
+      }
+    }).map(element => {
+      if(element.translated_sentence[0]== ' '){
+        return {
+          original_sentence: element.original_sentence,
+          translated_sentence: element.translated_sentence.substring(1)
+        }
+      } else {
+        return element
+      }
+    })
     return ApiClient.post(API_CREATE_ALIGNMENT_ENDPOINT, {
       idParallelText: parseInt(idParallelText),
       translationObjectsArray, translationObjectsArray,
