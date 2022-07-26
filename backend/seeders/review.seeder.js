7f17b17463f8c567b577fe92536fd8be99ebe698
Simone Venturi <s.venturi@sis-ter.it>
2022-07-26 15:05:32 +0200

    [SEEDER] Removed review seeds

--- backend/seeders/review.seeder.js
+++ backend/seeders/review.seeder.js
@@ -4,16 +4,5 @@ const Review = db.review;
 
 /* creation basic users */
 module.exports = {
-    createReviews : async () => {  
-        await Review.create({
-            translationId: 1,
-            translator: 1,
-            score: 4
-        });
-        await Review.create({
-            translationId: 1,
-            translator: 2,
-            score: 4
-        });
-    }
+    createReviews : async () => { }
 }
