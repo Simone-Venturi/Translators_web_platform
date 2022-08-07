f5671e2501f2328f40558127b6e860041d7064f9
Simone Venturi <s.venturi@sis-ter.it>
2022-08-07 18:19:15 +0200

    [FRONTEND] Add dataset store

---
+++ frontend/src/store/dataset.module.js
@@ -0,0 +1,48 @@
+import { parseQuery } from 'vue-router';
+import DatasetsService from '../services/dataset.service';
+
+export const dataset = {
+  namespaced: true,
+  state: {
+    datasets: null
+  },
+  actions: {
+    readAllDatasets({commit}){
+      return DatasetsService.getAllDatasets().then(
+        (datasets) => {
+          commit('readAllDatasetsSuccess', datasets);
+          return Promise.resolve(datasets);
+        },
+        (error) => {
+          commit('readAllDatasetsFailure');
+          return Promise.reject(error);
+        }
+      );
+    },
+    createDataset({dispatch}, payload){
+      console.log(payload)
+      return DatasetsService.createDataset(payload.datasetName, payload.datasetURL).then(
+        () =>{
+          dispatch('readAllDatasets');
+          return Promise.resolve();
+        },
+        (error) => {
+          return Promise.reject(error);
+        }
+      )
+    }
+  },    
+  mutations: {
+    readAllDatasetsSuccess(state, datasets) {
+      state.datasets = datasets.data;
+    },
+    readAllDatasetsFailure(state) {
+      state.datasets = null
+    }
+  },
+  getters: {
+    getAllDatasets(state){
+      return state.datasets
+    }
+  }
+};
