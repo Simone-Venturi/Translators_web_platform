cddbe660db08f5b41738bf2410e82a5587740058
Simone Venturi <s.venturi@sis-ter.it>
2022-08-24 12:14:49 +0200

    [FRONTEND] Add routes

--- frontend/src/services/dataset.service.js
+++ frontend/src/services/dataset.service.js
@@ -1,4 +1,11 @@
-import { ApiClient, API_ALL_DATASET_ENDPOINT, API_CREATE_DATASET_ENDPOINT, API_LOAD_RESOURCE_DATASET_ENDPOINT} from '@/services/app.services';
+import {
+  ApiClient,
+  API_ALL_DATASET_ENDPOINT,
+  API_CREATE_DATASET_ENDPOINT,
+  API_LOAD_RESOURCE_DATASET_ENDPOINT,
+  API_CHECK_DOWNLOAD_SIZE_DATASET_ENDPOINT,
+  API_DOWNLOAD_SIZE_DATASET_ENDPOINT
+} from '@/services/app.services';
 import authHeader from '@/services/auth-header';
 class DatasetsService {
   getAllDatasets() {
@@ -15,6 +22,26 @@ class DatasetsService {
       id: datasetID,
       file: file
     },{headers: { ...authHeader(), 'Content-Type': 'multipart/form-data'}})
+  }
+  checkDownloadSize(datasetID, languageFrom, languageTo, minReviewScore, maxReviewScore, atLeastAReview){
+    return ApiClient.post(API_CHECK_DOWNLOAD_SIZE_DATASET_ENDPOINT, {
+      dataset: datasetID,
+      languageFrom: languageFrom,
+      languageTo: languageTo,
+      minReviewScore: minReviewScore,
+      maxReviewScore: maxReviewScore,
+      atLeastAReview: atLeastAReview
+    },{headers: authHeader()})
+  }
+  downloadDataset(datasetID, languageFrom, languageTo, minReviewScore, maxReviewScore, atLeastAReview){
+    return ApiClient.post(API_DOWNLOAD_SIZE_DATASET_ENDPOINT, {
+      dataset: datasetID,
+      languageFrom: languageFrom,
+      languageTo: languageTo,
+      minReviewScore: minReviewScore,
+      maxReviewScore: maxReviewScore,
+      atLeastAReview: atLeastAReview
+    },{headers: authHeader()})
   }
 }
 export default new DatasetsService();
