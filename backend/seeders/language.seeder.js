3c2b942698959bdbe900f7ee3905cf12e0c0aac4
Simone Venturi <s.venturi@sis-ter.it>
2022-09-02 18:34:05 +0200

    [SEEDER] Modified seeders

--- backend/seeders/language.seeder.js
+++ backend/seeders/language.seeder.js
@@ -1,62 +1,20 @@
 const db = require("../models");
+const path = require("path");
+const { language } = require("../models");
 
 const Language = db.language;
 
 /* creation basic users */
 module.exports = {
-    createLanguages : async () => {  
-        await Language.create({
-            idlanguage: 1,
-            title: "English",
-            abbreviation: "EN"
-        });
-        
-        await Language.create({
-            idlanguage: 2,
-            title: "Italian",
-            abbreviation: "IT"
-        });
-        
-        await Language.create({
-            idlanguage: 3,
-            title: "Chinese",
-            abbreviation: "ZH"
-        });
-        
-        await Language.create({
-            idlanguage: 4,
-            title: "Spanish",
-            abbreviation: "ES"
-        });
-        
-        await Language.create({
-            idlanguage: 5,
-            title: "French",
-            abbreviation: "FR"
-        });
-
-        await Language.create({
-            idlanguage: 6,
-            title: "Portuguese",
-            abbreviation: "PL"
-        });
-
-        await Language.create({
-            idlanguage: 7,
-            title: "German",
-            abbreviation: "DE"
-        });
-
-        await Language.create({
-            idlanguage: 8,
-            title: "Dutch",
-            abbreviation: "NL"
-        });
-
-        await Language.create({
-            idlanguage: 9,
-            title: "Romanian",
-            abbreviation: "RO"
-        });
+    createLanguages : () => {  
+        const fs = require('fs');
+        let rawdata = fs.readFileSync(path.resolve(__dirname,'../resources/languages.json'));
+        let languages = JSON.parse(rawdata);
+        languages.forEach(async language => {
+            await Language.create({
+                title: language.itemLabel.charAt(0).toUpperCase() + language.itemLabel.slice(1),
+                abbreviation: language.lang_code.toUpperCase()
+            })
+        })
     }
 }
