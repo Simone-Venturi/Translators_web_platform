0f83cf70d4beb45a7476342d0f09b856d6f0ccc7
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-10-22 21:54:37 +0200

    refactor: translations insert

--- backend/processes/postgrestranslation.process.js
+++ backend/processes/postgrestranslation.process.js
@@ -13,31 +13,33 @@ exports.update = async (job) => {
 async function massiveInsert(job, allLanguages){
     for (let record of job.data.records){
         let sentences = []
-        for await (let language of record.languages) {
-            let language_id = allLanguages.filter(lang => lang.abbreviation === language.toUpperCase())[0].idlanguage
-            const [sentence, created] = await Sentence.findOrCreate({            
-                where: {
-                    sentence: record.object[language],
-                    languageId: language_id
-                }
-            });
-            sentences = [...sentences, sentence]
-        }
-        for (let i = 0; i < sentences.length; i++) {
-            for (let j = i + 1; j < sentences.length; j++){
-                await Translation.findOrCreate({
+        try {
+            for await (let language of record.languages) {
+                let language_id = allLanguages.filter(lang => lang.abbreviation === language.toUpperCase())[0].idlanguage
+                const [sentence, created] = await Sentence.findOrCreate({            
                     where: {
-                        original: sentences[i].idsentence,
-                        translated: sentences[j].idsentence
-                    },
-                    defaults: {
-                        original: sentences[i].idsentence,
-                        translated: sentences[j].idsentence,
-                        translator: job.data.translator,
-                        dataset_id: job.data.dataset_id
+                        sentence: record.object[language],
+                        languageId: language_id
                     }
-                })
+                });
+                sentences = [...sentences, sentence]
             }
+            for (let i = 0; i < sentences.length; i++) {
+                for (let j = i + 1; j < sentences.length; j++){
+                    await Translation.findOrCreate({
+                        where: {
+                            original: sentences[i].idsentence,
+                            translated: sentences[j].idsentence,
+                            dataset_id: job.data.dataset_id
+                        },
+                        defaults: {
+                            translator: job.data.translator
+                        }
+                    })
+                }
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
     return 1
