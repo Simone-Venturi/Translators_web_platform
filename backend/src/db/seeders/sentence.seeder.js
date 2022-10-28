9b3b26f869b2f426188ec0916e9479b95c5f9349
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-10-28 21:27:13 +0200

    refactor: updated sentences seed

--- backend/src/db/seeders/sentence.seeder.js
+++ backend/src/db/seeders/sentence.seeder.js
@@ -7,52 +7,57 @@ module.exports = {
     createSentences : async () => {  
         await Sentence.create({
             sentence: "Are you going to translate the rest of the book?",
-            languageId: 1
+            languageId: 130
         });
         
         await Sentence.create({
             sentence: "The water felt delightful.",
-            languageId: 1
+            languageId: 130
         });
         
         await Sentence.create({
             sentence: "L'acqua era deliziosa",
-            languageId: 2
+            languageId: 215
         });
 
         await Sentence.create({
             sentence: "Il pane è caldo",
-            languageId: 2
+            languageId: 215
         });
 
         await Sentence.create({
             sentence: "Oggi ho pranzato alle 13:00",
-            languageId: 2
+            languageId: 215
         });
 
         await Sentence.create({
             sentence: "Today I had lunch at 1:00 PM",
-            languageId: 1
+            languageId: 130
         });
 
         await Sentence.create({
             sentence: "你好",
-            languageId: 3
+            languageId: 541
         });
 
         await Sentence.create({
             sentence: "Hello",
-            languageId: 1
+            languageId: 130
         });
 
         await Sentence.create({
             sentence: "Ciao",
-            languageId: 2
+            languageId: 215
         });
         
         await Sentence.create({
             sentence: "Olá",
-            languageId: 6
+            languageId: 385
+        });
+
+        await Sentence.create({
+            sentence: "Hola",
+            languageId: 133
         });
     }
 }
