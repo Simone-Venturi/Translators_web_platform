f230f284466f4e38a9b7bce161f021b932a86895
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-11-23 17:31:24 +0100

    feat: add Swagger doc for /api/auth/signup route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -8,6 +8,70 @@ module.exports = function(app) {
     );
     next();
   });
+  /**
+   * @openapi
+   * '/api/auth/signup':
+   *  post:
+   *    tags:
+   *      - Signup
+   *    summary: Register a user
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              username:
+   *                type: string
+   *              password:
+   *                type: string
+   *              email:
+   *                type: string
+   *              roleId:
+   *                type: number
+   *                default: 1
+   *            required:
+   *              - username
+   *              - email
+   *              - password
+   *            example:
+   *              username: simone
+   *              password: strongPassword
+   *              email: simone@email.com
+   *    responses:
+   *        200:
+   *          description: Success
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: string
+   *                example:
+   *                  message: User was registered successfully!
+   *        400:
+   *          description: Conflict
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: string
+   *                example:
+   *                  message: Failed! Email is already in use!
+   *        500:
+   *          description: Internal Server Error
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: Error
+   */
   app.post(
     "/api/auth/signup",
     [
