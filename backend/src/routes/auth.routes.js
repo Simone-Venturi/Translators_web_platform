305895ac04bea255887cfbc0a06370ccfc33f715
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-11-23 17:53:56 +0100

    feat: add Swagger doc for /api/auth/signin route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -37,7 +37,7 @@ module.exports = function(app) {
    *              - password
    *            example:
    *              username: simone
-   *              password: strongPassword
+   *              password: strongHashedPassword
    *              email: simone@email.com
    *    responses:
    *        200:
@@ -79,5 +79,85 @@ module.exports = function(app) {
     ],
     controller.signup
   );
+  /**
+   * @openapi
+   * '/api/auth/signin':
+   *  post:
+   *    tags:
+   *      - Signin
+   *    summary: Login a user
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            properties:
+   *              username:
+   *                type: string
+   *              password:
+   *                type: string
+   *            required:
+   *              - username
+   *              - password
+   *            example:
+   *              username: simone
+   *              password: strongHashedPassword
+   *    responses:
+   *        200:
+   *          description: Success
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  id: 
+   *                    type: number
+   *                  username: 
+   *                    type: string
+   *                  email: 
+   *                    type: string
+   *                  roles: 
+   *                    type: object
+   *                    properties:
+   *                      role_translator:
+   *                        type: boolean
+   *                      role_data_scientist:
+   *                        type: boolean
+   *                      role_admin:
+   *                        type: boolean
+   *                  accessToken:
+   *                    type: string
+   *                example:
+   *                  id: 1
+   *                  username: simone
+   *                  email: simone@email.com
+   *                  roles:
+   *                      role_translator: true
+   *                      role_data_scientist: true
+   *                      role_admin: true
+   *                  accessToken: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjY5MjIxODgzLCJleHAiOjE2NjkzMDgyODN9.2rh_1bFBeYraAMi5OtlU0OKswBfn2Xqj4vEAC9J7JQg
+   *        401:
+   *          description: Unauthorized
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: string
+   *                example:
+   *                  message: Invalid Password!
+   *                  accessToken: null
+   *        500:
+   *          description: Internal Server Error
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: Error
+   */
   app.post("/api/auth/signin", controller.signin);
 };
