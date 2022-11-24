111ff069b2e3d05d0e46298c7fe7973da419d271
Simone Venturi <simone.venturi5@studio.unibo.it>
2022-11-24 20:02:12 +0100

    feat: add Swagger doc for review routes

--- backend/src/routes/review.routes.js
+++ backend/src/routes/review.routes.js
@@ -8,6 +8,48 @@ module.exports = function(app) {
     );
     next();
   });
+  /**
+   * @openapi
+   * '/api/review/create':
+   *  post:
+   *    security:
+   *      - ApiKeyAuth: []
+   *    tags:
+   *      - Review a translation
+   *    summary: Translation review
+   *    requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            properties:
+   *              idTranslation:
+   *                type: integer
+   *              rateReview:
+   *                type: integer
+   *            required:
+   *              - idTranslation
+   *              - rateReview
+   *            example:
+   *              idTranslation: 1
+   *              rateReview: 5
+   *    responses:
+   *        200:
+   *          description: Success
+   *        403:
+   *          description: Unauthorized
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message: 
+   *                    type: string
+   *                example:
+   *                  message: Unauthorized!
+   *        500:
+   *          description: Internal Server Error
+   */
   app.post(
     "/api/review/create",
     [authJwt.verifyToken, authJwt.isTranslator],
